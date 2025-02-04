c08b29686f7e37fb79062ee5bef74e48dfb2405b
Anastasiia Shcherbakova <anastasiia.shcherbakova@icr.ac.uk>
2025-02-04 14:33:12 +0000

    Specified shared_path in my bashrc file for debugging

--- run-GA-Alma.sh
+++ run-GA-Alma.sh
@@ -3,10 +3,9 @@
 source ~/.bashrc
 
 userCWD="$(pwd)"
-shared_path=$(cat /data/rds/DIT/SCICOM/SCRSE/shared/apps/GreenAlgorithms4HPC/shared_path.txt)
 
-cd "${shared_path}apps/GreenAlgorithms4HPC/"
+cd "${shared_path}/apps/GreenAlgorithms4HPC/"
 
-mamba activate "${shared_path}conda/GA_env"
-python3 "${shared_path}apps/GreenAlgorithms4HPC/__init__.py" "$@" --userCWD "$userCWD"
+mamba activate "${shared_path}/conda/GA_env"
+python3 "${shared_path}/apps/GreenAlgorithms4HPC/__init__.py" "$@" --userCWD "$userCWD"
 mamba deactivate
